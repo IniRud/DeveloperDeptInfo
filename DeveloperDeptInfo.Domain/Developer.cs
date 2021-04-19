e0a6f1b35db8062c23d074459a50423d8abbe387
iniekan <iniakpan.e@gmail.com>
2021-04-18 20:38:10 -0600

    Add project files.

---
+++ DeveloperDeptInfo.Domain/Developer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperDeptInfo.Domain
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string DeveloperName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public DateTime JoinDate { get; set; }
+
+    }
+}
---
+++ DeveloperDeptInfo.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DeveloperDeptInfo.Services
+{
+    public class Class1
+    {
+    }
+}
