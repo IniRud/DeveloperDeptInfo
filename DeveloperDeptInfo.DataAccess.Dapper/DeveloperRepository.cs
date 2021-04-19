26fb8e1098384ad3a77d8b492f7a8dccd97cbb74
iniekan <iniakpan.e@gmail.com>
2021-04-18 21:12:34 -0600

    Adding Data Access layer

---
+++ DeveloperDeptInfo.DataAccess.Dapper/DeveloperRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperDeptInfo.DataAccess.Dapper
+{
+    class DeveloperRepository
+    {
+        protected readonly IConfiguration _config; 
+
+        public DeveloperRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //Create an IDB connection object to 
+        
+    }
+}
---
+++ DeveloperDeptInfo.DataAccess.Dapper/IDeveloperRepository.cs
@@ -0,0 +1,19 @@
+﻿using DeveloperDeptInfo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperDeptInfo.DataAccess.Dapper
+{
+    public interface IDeveloperRepository
+    {
+        Task<IEnumerable<Developer>> GetAllDeveloperAsync();
+        Task<Developer> GetDeveloperByIdAsync(int Id);
+        Task<Developer> GetDeveloperByEmail(string Email);
+        void AddDeveloper(Developer developer);
+        void UpdateDeveloper(Developer developer);
+        void DeleteDeveloper(int id);
+    }
+}
